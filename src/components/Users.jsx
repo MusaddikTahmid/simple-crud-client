76692ac0277eada6abd242298dee4a22ac5067a2
MusaddikTahmid <musaddiktahmid@gmail.com>
2024-05-14 10:11:19 +0600

    delete mathod stared

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,14 +3,27 @@ import { useLoaderData } from "react-router-dom";
 const Users = () => {
   const users = useLoaderData();
 
+  const handleDelete = (_id) => {
+    console.log("seleted users", _id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <>
       <h2>{users.length}</h2>
       <div>
         {users.map((user) => (
           <div key={user._id}>
-            <h3>{user.name}</h3>
-            <p>{user.email}</p>
+            <p>User ID : {user._id}</p>
+            <h3>Name : {user.name}</h3>
+            <p>Email : {user.email}</p>
+            <button onClick={() => handleDelete(user._id)}>X</button>
           </div>
         ))}
       </div>
